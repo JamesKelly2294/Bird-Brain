ab04c3d025efab0af9607c0b3f9eafbe85c7c8e7
James Kelly <jameskelly@acd412290453237685704.wireless.rit.edu>
2017-10-06 14:55:05 -0400

    Initial commit

---
+++ Shitty Wizard/Assets/Scripts/EnemyController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour {
+
+	public float speed = 100.0f;
+	public Transform target;
+
+	Rigidbody2D m_Rigidbody;
+
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody2D> ();
+	}
+
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = target.position - 
+			transform.position;
+		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+
+		m_Rigidbody.AddForce (transform.up * speed);
+	}
+}
---
+++ Shitty Wizard/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public float speed = 300.0f;
+
+	Rigidbody2D m_Rigidbody;
+	Camera m_Camera;
+
+	// Use this for initialization
+	void Start () {
+		m_Rigidbody = GetComponent<Rigidbody2D> ();
+		m_Camera = Camera.main;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector3 direction = Input.mousePosition - 
+			Camera.main.WorldToScreenPoint(transform.position);
+		float angle = Mathf.Atan2(direction.y, direction.x) * Mathf.Rad2Deg;
+		transform.rotation = Quaternion.AngleAxis(angle - 90f, Vector3.forward);
+
+		float verticalInput = Input.GetAxis ("Vertical");
+		float horizontalInput = Input.GetAxis ("Horizontal");
+
+		m_Rigidbody.AddForce (Vector2.up * verticalInput * speed);
+		m_Rigidbody.AddForce (Vector2.right * horizontalInput * speed);
+	}
+}
