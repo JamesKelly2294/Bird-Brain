89982193235f03b4b6319d7ce50e5872ed3ee630
SirGeoffers <geoffersthethird@gmail.com>
2017-10-08 19:10:27 -0400

    stretchy camera

--- Shitty Wizard/Assets/Scripts/CameraController.cs
+++ Shitty Wizard/Assets/Scripts/CameraController.cs
@@ -7,16 +7,26 @@ public class CameraController : MonoBehaviour {
 	public Transform target;
 	public Vector3 offset = new Vector3 (0.0f, 0.0f, 18.0f);
 
+    private Camera cam;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        cam = GetComponent<Camera>();
+
+        cam.ResetProjectionMatrix();
+        var m = cam.projectionMatrix;
+
+        m.m11 *= 1.5f;
+        cam.projectionMatrix = m;
+    }
 	
 	// Update is called once per frame
 	void Update () {
+
 		transform.position = new Vector3 (
 			target.transform.position.x, 
 			transform.position.y,
 			target.transform.position.z) + offset;
-	}
+
+    }
 }
---
+++ Shitty Wizard/Assets/Scripts/Jeff/SimpleCharacterController.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+public class SimpleCharacterController : MonoBehaviour {
+
+    public bool useJoystick;
+    public float moveSpeed;
+    public GameObject sprite;
+
+    private CharacterController cc;
+
+	// Use this for initialization
+	void Start () {
+
+        cc = this.GetComponent<CharacterController>();
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        float inH = Input.GetAxis("Horizontal");
+        float inV = Input.GetAxis("Vertical");
+
+        if (useJoystick) {
+
+        } else {
+            
+        }
+
+        Vector3 moveVec = new Vector3(inH, 0, inV).normalized * moveSpeed;
+        cc.SimpleMove(moveVec);
+
+	}
+}
