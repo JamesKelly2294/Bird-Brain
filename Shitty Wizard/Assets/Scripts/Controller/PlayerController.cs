8a75171e00ae854be3a2c6d3e39ad0949dc8000a
MAIN\njf1994 <njfed@roadrunner.com>
2017-10-13 14:37:10 -0400

    Added ability to use 1, 2, 3, and 4 to switch between weapons

--- Shitty Wizard/Assets/Scripts/Controller/PlayerController.cs
+++ Shitty Wizard/Assets/Scripts/Controller/PlayerController.cs
@@ -4,8 +4,16 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
+	public bool weapon1 = true;
+	public bool weapon2 = false;
+	public bool weapon3 = false;
+	public bool weapon4 = false;
 	public float speed = 0.4f;
+	Vector3 projectileDirection;
     public GameObject projectilePrefab;
+	public GameObject projectile2Prefab;
+	public GameObject projectile3Prefab;
+	public GameObject projectile4Prefab;
 
 	Rigidbody m_Rigidbody;
 	Transform m_SpriteTransform;
@@ -23,22 +31,57 @@ public class PlayerController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-        
+		// switching weapons
+		if(Input.GetKey(KeyCode.Alpha1)){
+			weapon1 = true;
+			weapon2 = false;
+			weapon3 = false;
+			weapon4 = false;
+		}
+		else if(Input.GetKey(KeyCode.Alpha2)){
+			weapon1 = false;
+			weapon2 = true;
+			weapon3 = false;
+			weapon4 = false;
+		}
+		else if(Input.GetKey(KeyCode.Alpha3)){
+			weapon1 = false;
+			weapon2 = false;
+			weapon3 = true;
+			weapon4 = false;
+		}
+		else if(Input.GetKey(KeyCode.Alpha4)){
+			weapon1 = false;
+			weapon2 = false;
+			weapon3 = false;
+			weapon4 = true;
+		}
+
         // Shooting
         if (Input.GetMouseButton(0)) {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            float rayDistance;
-            if (groundPlane.Raycast(ray, out rayDistance)) {
-                Vector3 projectileDirection = ray.origin + ray.direction * rayDistance - this.transform.position;
-                projectileDirection.y = 0;
-                projectileDirection = projectileDirection.normalized;
-                spell.RequestCast(projectileDirection);
-                //ProjectileBasic pBasic = ProjectileManager.CreateProjectile(projectilePrefab, EntityType.Player, this.gameObject, this.transform.position + Vector3.up * 0.5f) as ProjectileBasic;
-                //pBasic.Init(projectileDirection, 10.0f);
-            }
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			float rayDistance;
+			if (groundPlane.Raycast (ray, out rayDistance)) {
+				projectileDirection = ray.origin + ray.direction * rayDistance - this.transform.position;
+				projectileDirection.y = 0;
+				projectileDirection = projectileDirection.normalized;
+			}
 
+			if (weapon1 == true) {
+				spell.RequestCast (projectileDirection);
+				//ProjectileBasic pBasic = ProjectileManager.CreateProjectile(projectilePrefab, EntityType.Player, this.gameObject, this.transform.position + Vector3.up * 0.5f) as ProjectileBasic;
+				//pBasic.Init(projectileDirection, 10.0f);
+			} 
+			else if (weapon2 == true) {
+				
+			}
+			else if (weapon3 == true) {
+
+			}
+			else if (weapon4 == true) {
+
+			}
         }
-
     }
 
 	void FixedUpdate() {
