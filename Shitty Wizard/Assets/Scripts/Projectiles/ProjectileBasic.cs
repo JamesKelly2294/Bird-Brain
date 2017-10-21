1f41acf234333afd821c12a3de9a48c66daf42b4
SirGeoffers <geoffersthethird@gmail.com>
2017-10-21 17:30:59 -0400

    basic projectiles face direction moving

--- Shitty Wizard/Assets/Scripts/Projectiles/ProjectileBasic.cs
+++ Shitty Wizard/Assets/Scripts/Projectiles/ProjectileBasic.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 
 public class ProjectileBasic : Projectile {
 
+    public GameObject sprite;
+
     private Vector3 direction;
     private float speed;
 
@@ -16,6 +18,7 @@ public class ProjectileBasic : Projectile {
         this.direction = direction;
         this.speed = speed;
         this.lifetime = 10.0f;
+        this.sprite.transform.rotation = Quaternion.Euler(0, 0, -Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + 90);
     }
 
 }
