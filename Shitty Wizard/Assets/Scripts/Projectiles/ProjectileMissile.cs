7edeb3aca659ed075e5782378b3cadfaf8ce0dd1
Nick Federico <Nick Federico>
2017-10-23 18:29:15 -0400

    Magic Missile mostly working

--- Shitty Wizard/Assets/Scripts/Projectiles/ProjectileMissile.cs
+++ Shitty Wizard/Assets/Scripts/Projectiles/ProjectileMissile.cs
@@ -10,18 +10,14 @@ public class ProjectileMissile : Projectile {
 	private Vector3 direction;
 	private float speed;
 
-	// public float frequency = 20.0f;  // Speed of sine movement
-	// public float magnitude = 0.5f;   // Size of sine movement
-	// private Vector3 axis;
-
-	// private Vector3 pos;
-	// axis = transform.right;
+	public float frequency = 10.0f;  // Speed of sine movement
+	public float magnitude = 0.1f;   // Size of sine movement
+	private Vector3 axis;            // Sin wave axis
 
 	protected override void OnMove() {
-		this.transform.position = this.transform.position + direction * speed * Time.deltaTime;
-
-		// pos += transform.up * Time.deltaTime * MoveSpeed;
-		// transform.position = pos + axis * Mathf.Sin (Time.deltaTime * frequency) * magnitude;
+		axis = this.transform.forward;
+		this.transform.position += direction * Time.deltaTime * speed;
+		this.transform.position = this.transform.position + axis * Mathf.Sin (Time.time * frequency) * magnitude;
 	}
 
 	public void Init(Vector3 direction, float speed) {
