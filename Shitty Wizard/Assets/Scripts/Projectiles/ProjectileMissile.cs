dc926eca71e7ef81712fc2d7fc0d12dfeb9b41a2
Nick Federico <Nick Federico>
2017-10-23 19:02:00 -0400

    Weird errors bro

--- Shitty Wizard/Assets/Scripts/Projectiles/ProjectileMissile.cs
+++ Shitty Wizard/Assets/Scripts/Projectiles/ProjectileMissile.cs
@@ -24,6 +24,6 @@ public class ProjectileMissile : Projectile {
 		this.direction = direction;
 		this.speed = speed;
 		this.lifetime = 10.0f;
-		this.sprite.transform.rotation = Quaternion.Euler(0, 0, -Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + 90);
+		this.transform.rotation = Quaternion.Euler(0, 0, -Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + 90);
 	}
 }
