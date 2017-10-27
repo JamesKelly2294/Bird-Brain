e86676583cd6a98dc54f7a3e3255c173a8dd0a5e
Nick Federico <Nick Federico>
2017-10-26 20:05:24 -0400

    Added Owl Boss feather attack (currently attached to player as thrid weapon)

---
+++ Shitty Wizard/Assets/Scripts/Projectiles/ProjectileFeather.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileFeather : Projectile {
+
+	public GameObject sprite;
+
+	private Vector3 direction;
+	private float speed;
+
+	int timer;
+
+	void Start() {
+		timer = 0;
+	}
+
+	protected override void OnMove() {
+		timer++;
+
+		if (timer == 20) {
+			speed = speed / 6;
+		} 
+		else if(timer == 40){
+			speed = speed * 6;
+			timer = 0;
+		}
+
+		this.transform.position = this.transform.position + direction * speed * Time.deltaTime;
+	}
+
+	public void Init(Vector3 direction, float speed) {
+		this.direction = direction;
+		this.speed = speed;
+		this.lifetime = 10.0f;
+		this.sprite.transform.rotation = Quaternion.Euler(0, 0, -Mathf.Atan2(direction.x, direction.z) * Mathf.Rad2Deg + 90);
+	}
+}
---
+++ Shitty Wizard/Assets/Scripts/Projectiles/Spells/FeatherAttack.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FeatherAttack : Spell {
+
+	public GameObject featherPrefab;
+
+	public float circle;
+	public int numFeathers;
+
+	public override void Cast(Vector3 _dir) {
+
+		float offset = circle / (float)(numFeathers - 1);
+		float startRot = -circle / 2.0f;
+
+
+		// Play sound if there is one
+		//if (castSound != null) {
+
+		// If the sound is not already being played, play it
+		//if(!audioSource.isPlaying)
+		//{
+		//	//audioSource.clip = castSound;
+		//	audioSource.Play();
+		//}
+		AudioSource.PlayClipAtPoint(castSound, Camera.main.transform.position, volume);
+		//}
+
+		for (int i = 0; i < numFeathers; i++) {
+
+			float rot = startRot + offset * i;
+			Vector3 streamDir = Quaternion.Euler(0, rot, 0) * _dir;
+
+			ProjectileFeather pFthr = Projectile.Create(featherPrefab, EntityType.Player, owner, owner.transform.position + Vector3.up * 0.5f) as ProjectileFeather;
+			pFthr.Init(streamDir, 6);
+
+		}
+
+	}
+}
