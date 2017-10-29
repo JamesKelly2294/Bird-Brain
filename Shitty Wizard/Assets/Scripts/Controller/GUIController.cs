b80b93ecb3b3ddfa18d42e38f3b8944425778dbf
James Kelly <dxk2294@rit.edu>
2017-10-29 02:56:44 -0400

    Polish

--- Shitty Wizard/Assets/Scripts/Controller/GUIController.cs
+++ Shitty Wizard/Assets/Scripts/Controller/GUIController.cs
@@ -8,50 +8,70 @@ namespace ShittyWizard.Controller.Game
 {
 	public class GUIController : MonoBehaviour
 	{
-
+		[Header ("Player")]
 		public Image currentHealthBar;
-		public Image currentBossBar;
-        public Image currentBossBarBase;
 		public Image currentEXPbar;
 
 		public float currentEXP = 0f;
 		public float maxEXP = 100f;
 		public int level = 1;
 		public Text levelText;
+
+		[Header ("Boss")]
+		public GameObject currentBossBarGO;
+		public Image currentBossBar;
+		public Image currentBossBarBase;
+
+		[Header ("Minimap")]
 		public Text floorText;
 		public RawImage minimap;
 		public RectTransform minimapRectTransform;
 		public RectTransform minimapPlayerTracker;
+		public RectTransform minimapStaircaseTracker;
 
-		private GameObject playerGO;
-		private EntityPlayer player;
-        public GameObject bossGO;
-        private Owlman owlman;
+		[Header ("References")]
+		public GameObject playerGO;
+		public GameObject staircaseGO;
+		public GameObject owlGO;
+		public GameObject minimapGO;
+		public EntityPlayer player;
 
 		public WorldController worldController;
+
+		private bool showMinimap = true;
 
 		// Use this for initialization
 		void Start ()
 		{
 			currentEXP = 0f;
-			playerGO = GameObject.FindGameObjectWithTag ("Player");
 			player = playerGO.GetComponent<EntityPlayer> ();
 			UpdateHealthBar ();
-            HideBossBar();
 		}
 
-		public void UpdateForNewLevel(string floor) {
+		public void UpdateForNewLevel (string floor)
+		{
 			minimap.texture = worldController.ActiveLevel.GenerateMinimap ();
 
 			floorText.text = "Floor " + floor;
 		}
 
-		public void UpdateMinimap() {
+		public void UpdateMinimap ()
+		{
+			if (!showMinimap) {
+				return;
+			}
 			float x = (playerGO.transform.position.x / minimap.texture.width) * minimapRectTransform.rect.width;
 			float z = (playerGO.transform.position.z / minimap.texture.height) * minimapRectTransform.rect.height;
-			float offset = (float)minimapRectTransform.localScale.x / 2;
+			float offsetX = (float)minimapRectTransform.localScale.x / 2 + 1.0f;
+			float offsetY = (float)minimapRectTransform.localScale.y / 2 - 1.0f;
 
-			minimapPlayerTracker.localPosition = new Vector3 (x + offset, z + offset, 0.0f);
+			if (staircaseGO != null) {
+				minimapPlayerTracker.localPosition = new Vector3 (x + offsetX, z + offsetY, 0.0f);
+
+				x = (staircaseGO.transform.position.x / minimap.texture.width) * minimapRectTransform.rect.width;
+				z = (staircaseGO.transform.position.z / minimap.texture.height) * minimapRectTransform.rect.height;
+				minimapStaircaseTracker.localPosition = new Vector3 (x + offsetX, z + offsetY, 0.0f);
+			}
 		}
 
 		// Update is called once per frame
@@ -65,7 +85,7 @@ namespace ShittyWizard.Controller.Game
 
 			if (currentEXP >= maxEXP) {
 				level = level + 1;
-				levelText.text = "Level " + level;
+				levelText.text = level.ToString ();
 				float tempXP = currentEXP;
 				currentEXP = tempXP - maxEXP;
 			}
@@ -79,32 +99,40 @@ namespace ShittyWizard.Controller.Game
 			if (player.maxHealth <= 0) {
 				ratio = 1;
 			}
-			currentHealthBar.rectTransform.sizeDelta = new Vector2 (ratio * 250f, 20f);
+			currentHealthBar.rectTransform.sizeDelta = new Vector2 (ratio * 250.0f, 20f);
 
-            if(currentBossBar.enabled == true)
-            {
-                /*float bossratio = owlman.health / owlman.maxHealth;
-                if (owlman.maxHealth <= 0)
-                {
-                    ratio = 1;
-                }
-                currentBossBar.rectTransform.sizeDelta = new Vector2(bossratio * 250f, 20f);*/
-            }
+			if (owlGO != null) {
+				Owlman owlman = owlGO.GetComponent<Owlman> ();
+				float bossratio = owlman.health / owlman.maxHealth;
+				if (owlman.maxHealth <= 0) {
+					currentBossBarGO.SetActive (false);
+					ratio = 1;
+				}
+				currentBossBar.rectTransform.localPosition = new Vector3 ((bossratio * 250.0f) - 250.0f, currentBossBar.rectTransform.localPosition.y, currentBossBar.rectTransform.localPosition.z);
+				currentBossBar.rectTransform.sizeDelta = new Vector2 (bossratio * 500.0f, 20f);
+			}
 		}
 
-        public void MakeBossBarActive()
-        {
-            currentBossBarBase.enabled = true;
-            currentBossBar.enabled = true;
-            //bossGO = GameObject.Find("Owl Boss");
-            owlman = bossGO.GetComponent<Owlman>();
-        }
+		public void SetBoss (GameObject bossGO)
+		{
+			owlGO = bossGO;
+			showMinimap = false;
+			minimapGO.SetActive (false);
+			ShowBossBar ();
+		}
 
-        public void HideBossBar()
-        {
-            currentBossBarBase.enabled = false;
-            currentBossBar.enabled = false;
-        }
+		public void ShowBossBar ()
+		{
+			currentBossBarGO.SetActive (true);
+		}
+
+		public void HideBossBar ()
+		{
+			currentBossBarGO.SetActive (false);
+			showMinimap = true;
+			minimapGO.SetActive (true);
+			owlGO = null;
+		}
 
 		public void GiveEXP (float EXP)
 		{
